8d05caf7b4b647856a7a91a07fb436f43caac7e2
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-05-25 21:10:06 -0400

    added API paths in basic

---
+++ basicpermissions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BasicpermissionsConfig(AppConfig):
+    name = 'basicpermissions'
---
+++ jwtdemo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JwtdemoConfig(AppConfig):
+    name = 'jwtdemo'
