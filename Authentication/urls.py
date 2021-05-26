499d70c396fcad87941cb0b0febbf69002bcca67
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-05-26 17:42:03 -0400

    done with jwt with future verisons possible.

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/jwtauth/', include('jwtauth.urls'), name='jwtauth'),
 ]
