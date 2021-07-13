b9b2a293ff656a0d8c1197e6b4058ff56ae92b82
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-07-13 00:34:55 -0400

    added jwt to app

--- jwtdemo/urls.py
+++ jwtdemo/urls.py
@@ -1,5 +1,5 @@
 # from django.urls import path
-# from .views import registration
+# 
 
 # urlpatterns = [
 #     path('register/', registration, name='register')
@@ -8,13 +8,14 @@
 from django.conf.urls import url
 from django.conf.urls import include
 from rest_framework import urlpatterns
-
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
-
+from .views import registration
 router = DefaultRouter()
 router.register('user-viewset', views.UserViewset, basename='user-viewset')
 
 urlpatterns =[
-    url(r'',include(router.urls))
+    url(r'',include(router.urls)),
+    path('register/', registration, name = 'register')
 ]
--- jwtdemo/views.py
+++ jwtdemo/views.py
@@ -12,7 +12,7 @@ from rest_framework_simplejwt.tokens import RefreshToken, Token
 from .serializers import UserSerializer
 from rest_framework.authentication import TokenAuthentication
 from . import models
-from .import permissions
+from .import permissions1
 from jwtdemo import serializers
 from rest_framework import filters
 # from django.http import Http404
@@ -22,31 +22,30 @@ from rest_framework import filters
 # from rest_framework.response import Response
 # from rest_framework.views import APIView
 # from api.serializers import ProductSerializer, CatogorySerializer
-# from shop.models import Product, Category
 
 
 User = get_user_model()
 
-# @decorators.api_view(["POST"])
-# @decorators.permission_classes([permissions.AllowAny])
-# def registration(request):
-#     serializer = UserCreateSerializer(data = request.data)
-#     if not serializer.is_valid():
-#         return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-#     user = serializer.save()
-#     refresh = RefreshToken.for_user(user)
+@decorators.api_view(["POST"])
+@decorators.permission_classes([permissions.AllowAny])
+def registration(request):
+    serializer = UserSerializer(data = request.data)
+    if not serializer.is_valid():
+        return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    user = serializer.save()
+    refresh = RefreshToken.for_user(user)
 
-#     res = {
-#         "refresh": str(refresh),
-#         "access":str(refresh.access_token),
-#     }
+    res = {
+        "refresh": str(refresh),
+        "access":str(refresh.access_token),
+    }
 
-#     return response.Response(res, status.HTTP_201_CREATED)
+    return response.Response(res, status.HTTP_201_CREATED)
 
 class UserViewset(viewsets.ViewSet):
     
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,)
+    permission_classes = (permissions1.UpdateOwnProfile,)
     serializer_class = UserSerializer
     filter_backends =(filters.SearchFilter,)
     search_fields = ('email','first_name','last_name')
