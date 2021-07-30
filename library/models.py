205fd520b6ff757c7f482110fb722dace4a782bf
muhammadsidd <muhammadsidd@mail.usf.edu>
2021-07-29 23:57:15 -0400

    annotatation documentation added

--- library/models.py
+++ library/models.py
@@ -22,7 +22,8 @@ class AuthorManager(models.Manager):
 
     def annotate_with_copies_sold(self):
         return self.get_queryset().annotate_with_copies_sold()
-
+#However you will no be able to use it on QuerySet object like:
+#author_books = Author.objects.filter(id=2).total_copies_sold()
 class AuthorQuerySet(models.QuerySet):
     def annotate_with_copies_sold(self):
         return self.annotate(copies_sold=Sum('books__copies_sold'))
